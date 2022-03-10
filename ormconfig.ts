e833cf62e37fba5fb5c5f344b29dfe5b6d6f181a
Raihan Parama <raihanpl10@gmail.com>
2022-03-10 20:17:39 +0700

    fix: remove ormconfig due to bug

--- ormconfig.ts
+++
@@ -1,9 +0,0 @@
-import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
-
-const config  = {
-    type: process.env.TYPEORM_CONNECTION,
-    
-
-}
-
-export default config;
