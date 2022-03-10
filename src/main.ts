f0db66c8870ffb3fba18f2f0964e13c4a1dc61bf
Raihan Parama <raihanpl10@gmail.com>
2022-03-10 22:13:29 +0700

    fix:try to fix cors error

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     new ClassSerializerInterceptor(app.get(Reflector))
   );
   app.use(helmet());
-  app.enableCors({preflightContinue: true, credentials: true});
+  app.enableCors({preflightContinue: true, credentials: true,  origin: /.+/ });
   const config = new DocumentBuilder()
     .setTitle('Filkom Academy')
     .setDescription('The Filkom Academy API')
